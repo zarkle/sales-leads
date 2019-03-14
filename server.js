a4a7d5ee56d740047ac84310da10cab7745757d2
Beverly Pham <beverly.pham@gmail.com>
2019-03-13 23:09:48 -0700

    make server.js instead of bin/www for recreation of express-generator

---
+++ server.js
@@ -0,0 +1,64 @@
+const app = require('./app');
+const debug = require('debug')('sales-leads:server');
+const http = require('http');
+
+// get port from environment variables
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// create HTTP server
+const server = http.createServer(app);
+
+// Listen on port, on all network interfaces
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+// normalize a port into a number, string, or false
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+// event listener for HTTP server "error" event
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly message
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+// event listener for HTTP server "listening" event
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
