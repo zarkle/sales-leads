24603016237ec1bb7418ee393f921bc6aca7546e
Beverly Pham <beverly.pham@gmail.com>
2019-03-19 14:35:15 -0700

    make controller folder, and make controller for index function for modularity

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Express'});
-});
+let index = require('../controllers/index');
+router.get('/', index.index);
 
 module.exports = router;
