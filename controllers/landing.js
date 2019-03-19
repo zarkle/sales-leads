20d7232325af1c51ce34d550c7a23b3b041d7ad9
Beverly Pham <beverly.pham@gmail.com>
2019-03-19 15:16:15 -0700

    landing page post route added to submit email

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.get_landing = (req, res, next) => {
   res.render('landing', {title: 'Express'});
 }
+
+exports.submit_lead = (req, res, next) => {
+  console.log('lead email:', req.body.lead_email);
+  res.redirect('/');  // go back to landing page after submit form
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const landing = require('../controllers/landing');
+
 router.get('/', landing.get_landing);
+
+router.post('/', landing.submit_lead);
 
 module.exports = router;
