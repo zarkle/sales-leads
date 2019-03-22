14e555c28ecae73f996757b39b4f216958f88df8
Beverly Pham <beverly.pham@gmail.com>
2019-03-21 20:59:55 -0700

    implement leads route, show collection of leads after enter email,

--- controllers/landing.js
+++ controllers/landing.js
@@ -5,10 +5,15 @@ exports.get_landing = (req, res, next) => {
 };
 
 exports.submit_lead = (req, res, next) => {
-
   return models.Lead.create({
     email: req.body.lead_email
   }).then( lead => {
-    res.redirect('/');  // go back to landing page after submit form
+    res.redirect('/leads');  // go back to landing page after submit form
+  })
+};
+
+exports.show_leads = (req, res, next) => {
+  return models.Lead.findAll().then( leads => {
+    res.render('landing', {title: 'Express', leads: leads});
   })
 };
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,6 @@ router.get('/', landing.get_landing);
 
 router.post('/', landing.submit_lead);
 
+router.get('/leads', landing.show_leads);
+
 module.exports = router;
